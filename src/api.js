43eec26be8d13370632b7b3512b64574bb2b7c3b
seongbokim <rkdtlgn0@naver.com>
2025-12-31 15:17:34 +0900

    seongbo commit

---
+++ src/api.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE || "/api",
+});
+
+export async function fetchUsers() {
+  const { data } = await api.get("/users");
+  return data;
+}
+
+export async function fetchRecommendations(userId, limit = 12) {
+  const { data } = await api.get("/recommend", {
+    params: { user_id: userId, limit },
+  });
+  return data;
+}
+
+export async function fetchMovies() {
+  const { data } = await api.get("/movies?limit=100");
+  return data;
+}
+
+export default api;
